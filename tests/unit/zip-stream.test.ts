714ece3a76ad85de80678507629d3b42217acebd
EitaPreulaJhol <carlos.pdesanti@gmail.com>
2026-09-18 12:00:54 -0300

    Chore: drop unnecessary eslint-disable in zip-stream test

--- tests/unit/zip-stream.test.ts
+++ tests/unit/zip-stream.test.ts
@@ -37,7 +37,6 @@ async function collect(
   const reader = stream.getReader()
   const chunks: Uint8Array[] = []
   let total = 0
-  // eslint-disable-next-line no-constant-condition
   while (true) {
     const { done, value } = await reader.read()
     if (done) break
